49dd02622ba43893e19a978c454fc14a3b20a1b6
tylergreason <tyler.greason@protonmail.com>
2020-02-03 08:36:00 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  # before  validation is checked, the title is made to be title case by our method below, #make_title_case.
+  before_validation :make_title_case
+  # validation is checked _after_ that, and because of our method, the title should always be in title case. 
+
   private
 
   def is_title_case
